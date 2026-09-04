09fd890be458b2380b75438ac4cfc3fd69f30c0b
Malith Gamage <wgmgeewinda@gmail.com>
2026-09-04 12:28:44 +0530

    testimonials

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ let menu = document.querySelector(".menu-icon");
 menu.onclick = () => {
 menu.classList.toggle("move");
 };
+var swiper = new Swiper('.testimonial-content', {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 7500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+
+      });
