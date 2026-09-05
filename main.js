5f2d1c88db12a77cffb67445e8b0c27b8cc7b9ae
Malith Gamage <wgmgeewinda@gmail.com>
2026-09-05 11:01:19 +0530

    navbar responsiveness update

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
 menu.onclick = () => {
-menu.classList.toggle("move");
+    menu.classList.toggle("move");
+    navbar.classList.toggle("menu-open");
 };
+
 let header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
 header.classList.toggle('shadow', window.scrollY > 0);
 });
+
 var swiper = new Swiper('.testimonial-content', {
         spaceBetween: 30,
         centeredSlides: true,
