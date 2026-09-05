2799ab8c38ede4f41f7de9beb26cfc4e991dcdb0
Malith Gamage <wgmgeewinda@gmail.com>
2026-09-05 08:37:39 +0530

    header updates js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ let menu = document.querySelector(".menu-icon");
 menu.onclick = () => {
 menu.classList.toggle("move");
 };
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+header.classList.toggle('shadow', window.scrollY > 0);
+});
 var swiper = new Swiper('.testimonial-content', {
         spaceBetween: 30,
         centeredSlides: true,
